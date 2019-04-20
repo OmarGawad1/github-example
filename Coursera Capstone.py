f7c5f5cdbc4fa4265d06de6ce1c0bac352cd6cce
OmarGawad1 <49610157+OmarGawad1@users.noreply.github.com>
2019-04-20 23:55:29 +0200

    Add files via upload

---
+++ Coursera Capstone.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# ## This notebook will be mainly used for the capstone project
+
+# In[3]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[5]:
+
+
+print ('Hello Capstone Project Course!')
+
